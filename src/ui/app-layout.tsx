06f03aaa8cc1fdcd9c1e3adfc5da8bc8380e6b60
Hasan Ali <122746025+dev-hans-programmer@users.noreply.github.com>
2025-04-29 19:34:06 +0000

    Added a loading indicator

--- src/ui/app-layout.tsx
+++ src/ui/app-layout.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigation } from "react-router-dom"
 import CartOverview from "../features/cart/cart-overview"
 import Header from "./header"
+import Loader from "./loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading'
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
