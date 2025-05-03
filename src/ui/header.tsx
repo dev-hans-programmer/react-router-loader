0bcf0cf205051f33a68afb2763aaac8b54b5c692
Hasan Ali <122746025+dev-hans-programmer@users.noreply.github.com>
2025-05-03 10:56:00 +0000

    Styled texts

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -3,8 +3,8 @@ import SearchOrder from "../features/order/search-order";
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/"> Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest"> Fast React Pizza Co.</Link>
       <p>By Hasan</p>
       <SearchOrder />
     </header>
